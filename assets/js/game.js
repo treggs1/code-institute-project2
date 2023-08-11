b318b7ef292afa005225486ad8456af0e5aa21d5
Paul Treggiden <treggs1@gmail.com>
2023-08-11 01:44:23 +0000

    Add letters as divs and start js functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,24 @@
 let words = ["testOne", "testTwo", "testThree", "testFour"];
 
-let game = {
+const game = {
     currentWord: "",
     currentWordArray: [],
     letterElements: [],
     lettersRemaining: 0,
 };
 
+const sButton = document.querySelector("button");
+sButton.addEventListener("click", startGame);
+
+function startGame() {
+    console.log("testOutput");
+    game.currentWord = "";
+    game.currentWordArray = "";
+    game.lettersremaining = 0;
+    getWord();
+}
+
+function getWord() {
+    let gameWord = Math.random() * 4;
+    console.log(gameWord);
+}
