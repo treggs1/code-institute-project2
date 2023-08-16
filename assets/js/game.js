87c273630a9e7dde775895483f1d1fc81c80fa85
Paul Treggiden <treggs1@gmail.com>
2023-08-16 06:01:22 +0000

    Add event listeners to letters and changes to make checker function work correctly

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,6 +2,7 @@ let words = ["testOne", "testTwo", "testThree", "testFour"];
 
 const game = {
     currentWord: "",
+    currentSolution: "",
     currentWordArray: [],
     letterElements: [],
     lettersRemaining: 0,
@@ -15,27 +16,29 @@ sButton.addEventListener("click", startGame);
 function startGame() {
     console.log("testOutput");
     game.currentWord = "";
-    game.currentWordArray = "";
+    game.currentWordArray = [];
     game.lettersremaining = 0;
     getWord();
     createPuzzle();
     setEventLis();
 }
-/* 
+ 
 function setEventLis() {
     let elements = document.querySelectorAll("#lettrs");
     elements.forEach((item) => {
-        item.addEventListener('click', checker())
+        item.addEventListener('click', function() {
+            checker(item, item.textContent);
+        });
     });
 }    
-*/
+
 function getWord() {
     let num = Math.floor(Math.random() * 4);
     game.currentWord = words[num];
     console.log(game.currentWord);
     console.log(num);
-    game.currentWordArray = game.currentWord.split("");
-    console.log(game.currentWordArray);
+    game.currentSolution = game.currentWord.split("");
+    console.log(game.currentSolution);
 }
 
 function createPuzzleElements(elementType, parentEl, output, cls) {
@@ -48,7 +51,7 @@ function createPuzzleElements(elementType, parentEl, output, cls) {
 
 function createPuzzle() {
     wordOut.innerHTML = "";
-    game.currentWordArray.forEach((letr) => {
+    game.currentSolution.forEach((letr) => {
         let div = createPuzzleElements("div", wordOut, "-", "puzzle_letters");
         if (letr == " ") {
             div.style.borderColor = "white";
@@ -64,8 +67,25 @@ function createPuzzle() {
 }
 
 function checker(element, checkLett) {
-    element.classList.remove("letters_norm");
-    element.classList.add("letters_correct");
+    let isCorrect = false;
+    
+    game.currentSolution.forEach((lett, index) => {
+        lett = lett.toString();
+        if (lett.toUpperCase() === checkLett) {
+            game.currentWordArray[index].textContent = checkLett;
+            isCorrect = true;
+        }
+    });
+
+    if (isCorrect) {
+        element.classList.remove("letters_norm");
+        element.classList.add("letters_correct");
+    } else {
+        element.classList.remove("letters_norm");
+        element.classList.add("letters_wrong");
+    }
     console.log(checkLett);
     console.log(element);
+    console.log(game.currentWordArray);
+    console.log(wordOut);
 }
